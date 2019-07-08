520985fa7b4667590db250f435a87c8c71eb7856
cesargdm <cesarguadarrama@icloud.com>
2019-07-08 14:24:19 -0500

    Remove commented code

--- packages/js-scripts/scripts/lint.js
+++ packages/js-scripts/scripts/lint.js
@@ -1,18 +1,8 @@
 'use strict'
 
-// const fs = require('fs')
 const chalk = require('chalk')
-// const path = require('path')
-// const promisify = require('util').promisify
-// const glob = require('glob')
-const eslint = require('eslint')
-const stylelint = require('stylelint')
 
 const spawn = require('cross-spawn')
-// const prettierStylelint = require('@cesargdm/prettier-stylelint')
-
-// const readFile = promisify(fs.readFile)
-// const writeFile = promisify(fs.writeFile)
 
 process.on('unhandledRejection', err => {
   throw err
@@ -24,16 +14,16 @@ const eslintConfig = `${paths.ownPath}/config/.eslintrc.json`
 const jsExtensions = 'js|jsx'
 const cssExtensions = 'css|pcss|scss'
 
-// const eslintProc = spawn.sync('eslint', ['src/**/*.js', '--fix'], {
-//   stdio: 'inherit',
-// })
-//
-// if (eslintProc.error) {
-//   process.error(eslintProc.error)
-//   process.exit(1)
-// } else {
-//   console.log(chalk.green('Eslint executed correctly'))
-// }
+const eslintProc = spawn.sync('eslint', ['src/**/*.js', '--fix'], {
+  stdio: 'inherit',
+})
+
+if (eslintProc.error) {
+  process.error(eslintProc.error)
+  process.exit(1)
+} else {
+  console.log(chalk.green('Eslint executed correctly'))
+}
 
 const stylelintProc = spawn.sync('stylelint', ['src/**/*.pcss', '--fix'], {
   stdio: 'inherit',
