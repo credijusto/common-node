2c06b15004f8bd16accd292725de35f422175104
cesargdm <cesarguadarrama@icloud.com>
2019-07-08 14:24:41 -0500

    Remove logs

--- packages/js-scripts/scripts/precommit.js
+++ packages/js-scripts/scripts/precommit.js
@@ -1,10 +1,6 @@
 const spawn = require('cross-spawn')
 const lintStaged = require.resolve('lint-staged')
 
-console.log()
-console.log('PRECOMMITING...', process.args)
-console.log()
-
 const paths = require('../config/paths')
 
 const config = `${paths.ownPath}/config/.lintstagedrc`
