dfce5566e888a45cdfe179c48be5c9e15f712132
cesargdm <cesarguadarrama@icloud.com>
2019-07-08 14:13:17 -0500

    Fix scripts

--- packages/js-scripts/scripts/lint.js
+++ packages/js-scripts/scripts/lint.js
@@ -1,79 +1,49 @@
 'use strict'
 
-const fs = require('fs')
+// const fs = require('fs')
 const chalk = require('chalk')
-const path = require('path')
-const promisify = require('util').promisify
-const glob = require('glob')
-const prettierEslint = require('prettier-eslint')
-const spawn = require('cross-spawn')
-const prettierStylelint = require('@cesargdm/prettier-stylelint')
+// const path = require('path')
+// const promisify = require('util').promisify
+// const glob = require('glob')
+const eslint = require('eslint')
+const stylelint = require('stylelint')
 
-const readFile = promisify(fs.readFile)
-const writeFile = promisify(fs.writeFile)
+const spawn = require('cross-spawn')
+// const prettierStylelint = require('@cesargdm/prettier-stylelint')
+
+// const readFile = promisify(fs.readFile)
+// const writeFile = promisify(fs.writeFile)
 
 process.on('unhandledRejection', err => {
   throw err
 })
 
 const paths = require('../config/paths')
+const eslintConfig = `${paths.ownPath}/config/.eslintrc.json`
 
 const jsExtensions = 'js|jsx'
 const cssExtensions = 'css|pcss|scss'
 
-const formatJSFiles = () =>
-  glob(`${paths.appPath}/src/**/*.?(${jsExtensions})`, (er, files) => {
-    const promises = files.map(file => {
-      const formatted = prettierEslint({
-        filePath: file,
-        eslintConfig: { extends: 'react-app' },
-      })
+// const eslintProc = spawn.sync('eslint', ['src/**/*.js', '--fix'], {
+//   stdio: 'inherit',
+// })
+//
+// if (eslintProc.error) {
+//   process.error(eslintProc.error)
+//   process.exit(1)
+// } else {
+//   console.log(chalk.green('Eslint executed correctly'))
+// }
 
-      return writeFile(file, formatted)
-    })
+const stylelintProc = spawn.sync('stylelint', ['src/**/*.pcss', '--fix'], {
+  stdio: 'inherit',
+})
 
-    Promise.all(promises)
-      .then(() => {
-        console.log(chalk.green('All js files formatted correctly'))
-      })
-      .catch(error => {
-        console.log('Error formating files')
-        console.error(error)
-      })
-  })
+console.log(stylelintProc)
 
-const formatsCSSFiles = () =>
-  glob(`${paths.appPath}/src/**/*.?(${cssExtensions})`, (error, files) => {
-    const formattings = files.map(file => {
-      const formatted = prettierStylelint({
-        filePath: file,
-        stylelintConfig: {
-          plugins: [require.resolve('stylelint-order')],
-          rules: {
-            'order/order': ['custom-properties', 'declarations'],
-            'order/properties-alphabetical-order': true,
-          },
-        },
-      })
-
-      return formatted
-    })
-
-    Promise.all(formattings).then(writes => {
-      const promises = writes.map((data, index) =>
-        writeFile(files[index], data)
-      )
-
-      Promise.all(promises)
-        .then(() => {
-          console.log(chalk.green('All css files formatted correctly'))
-        })
-        .catch(error => {
-          console.log('Error formating files')
-          console.error(error)
-        })
-    })
-  })
-
-formatJSFiles()
-formatsCSSFiles()
+if (stylelintProc.error) {
+  console.error(stylelintProc.error)
+  process.exit(1)
+} else {
+  console.log(chalk.green('Stylelint executed correctly'))
+}
