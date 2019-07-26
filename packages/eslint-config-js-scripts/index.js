f8cce91972abae658118eadbc3f02d26f6774030
cesargdm <cesarguadarrama@icloud.com>
2019-07-26 18:27:07 -0500

    Remove unused

--- packages/eslint-config-js-scripts/index.js
+++ packages/eslint-config-js-scripts/index.js
@@ -5,7 +5,7 @@
 module.exports = {
   parser: 'babel-eslint',
   extends: ['airbnb', 'plugin:jsx-a11y/strict'],
-  plugins: ['jsx-a11y', 'react-hooks', 'prettier'],
+  plugins: ['jsx-a11y', 'react-hooks'],
   env: {
     browser: true,
     node: true,
@@ -20,6 +20,5 @@ module.exports = {
     'react/jsx-filename-extension': 0,
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-    'prettier/prettier': 'error',
   },
 };
