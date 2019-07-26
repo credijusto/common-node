b4051c96b2ea68efb706e1b57ef00abc072d8635
cesargdm <cesarguadarrama@icloud.com>
2019-07-26 18:10:44 -0500

    Add prettier rules

--- packages/eslint-config-js-scripts/index.js
+++ packages/eslint-config-js-scripts/index.js
@@ -5,7 +5,7 @@
 module.exports = {
   parser: 'babel-eslint',
   extends: ['airbnb', 'plugin:jsx-a11y/strict'],
-  plugins: ['jsx-a11y', 'react-hooks'],
+  plugins: ['jsx-a11y', 'react-hooks', 'prettier'],
   env: {
     browser: true,
     node: true,
@@ -20,5 +20,6 @@ module.exports = {
     'react/jsx-filename-extension': 0,
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'prettier/prettier': 'error',
   },
 };
