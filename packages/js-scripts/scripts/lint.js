4cd88456f9e83b28c960866957d1000c36afb329
Michell Alegría <alegria.trm@gmail.com>
2020-03-06 09:51:34 -0600

    Skip non-existent file extensions (#32)
    * Skip non-existent file extensions
    
    * Remove console statements
    
    * Fix variable used in message

--- packages/js-scripts/scripts/lint.js
+++ packages/js-scripts/scripts/lint.js
@@ -21,17 +21,50 @@ console.log(chalk.green(' 🍃 Linting code ♻️\n'));
 
 const getCommaSeparated = (string) => string.replace(/\|/g, ',');
 
+const findFilesByExtension = async (extension) => {
+  return await execa('find', [
+    `./${sourceDir}`,
+    '-type',
+    'f',
+    '-name',
+    `*.${extension}`
+  ])
+}
+
+const hasFileTypes = async (extensions) => {
+  let hasFileType = false
+
+  const splittedExtensions = extensions.split('|')
+
+  for (const extention of splittedExtensions) {
+    if(!hasFileType) {
+      const { stdout: foundFiles} = await findFilesByExtension(extention)
+      hasFileType= Boolean(foundFiles)
+    }
+  }
+
+  return hasFileType
+}
+
 const tasks = new Listr(
   [
     {
       title: `Formatting ${otherFilesExtensions} (Prettier)`,
-      task: () =>
-        execa('prettier', [
+      task: async () => {
+        const hasOtherFilesExtensions = await hasFileTypes(otherFilesExtensions)
+
+        if (!hasOtherFilesExtensions) {
+          console.log(chalk.yellow(`No files of type ${otherFilesExtensions} were found`))
+          return undefined 
+        }
+
+        return execa('prettier', [
           `${sourceDir}/**/*.{${getCommaSeparated(otherFilesExtensions)}}`,
           isCIEnvironment ? '--check' : '--write',
           '--loglevel',
           'warn',
-        ]),
+        ])
+      },
     },
     {
       title: `Formatting ${jsExtensions}`,
@@ -60,8 +93,15 @@ const tasks = new Listr(
     },
     {
       title: `Formatting ${cssExtensions} (StyleLint)`,
-      task: () =>
-        new Listr([
+      task: async () => {
+        const hasCSS = await hasFileTypes(cssExtensions)
+
+        if (!hasCSS) {
+          console.log(chalk.yellow(`No files of type ${cssExtensions} were found`))
+          return undefined
+        }
+
+        return new Listr([
           {
             title: 'Prettier',
             task: () =>
@@ -80,7 +120,8 @@ const tasks = new Listr(
                 ...(isCIEnvironment ? [] : ['--fix']),
               ]),
           },
-        ]),
+        ])
+      },
     },
   ],
   { concurrent: true }
