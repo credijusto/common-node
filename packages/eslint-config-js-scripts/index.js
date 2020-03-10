af1dce7a26f7dbe8a822591497b8785bacedbaa1
Robert J. Kusznier <no-cheating@autistici.org>
2020-03-09 18:12:03 -0600

    Add curly rule to ESLint configuration

--- packages/eslint-config-js-scripts/index.js
+++ packages/eslint-config-js-scripts/index.js
@@ -33,5 +33,6 @@ module.exports = {
     'default-param-last': 'warn',
     'import/prefer-default-export': 'off',
     'func-style': ['error', 'declaration', { "allowArrowFunctions": true }],
+    'curly': 'all',
   },
 };
